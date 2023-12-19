b5d9c4cb3cd1c551e70c32e09ee08e58fabb33a8
Pinki Mandal <99741856+pinki-mandal@users.noreply.github.com>
2023-12-19 12:00:58 +0530

    Update Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -9,7 +9,7 @@ import 'swiper/css/scrollbar';
 // Import Swiper styles
 import 'swiper/css';
 
-const myFunction () => {
+export default  myFunction1 =() => {
   return (
     <Swiper
     modules={[Navigation, Pagination, Scrollbar, A11y]}
@@ -47,4 +47,4 @@ const myFunction () => {
   );
 };
 
-export default myFunction
+
